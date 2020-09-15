7cd34b40fc9909726b76af7af0775133aa1e73ed
nao.saino <saino305@gmail.com>
2020-09-15 09:57:47 +0000

    migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Book.create(inout:1,category:"給料",year:2021,month:5,amount:25)
+Book.create(inout:2,category:"家賃",year:2021,month:5,amount:5)
+Book.create(inout:2,category:"食費",year:2021,month:5,amount:3)
+Book.create(inout:2,category:"インフラ",year:2021,month:5,amount:3)
+Book.create(inout:2,category:"保険",year:2021,month:5,amount:3)
+Book.create(inout:2,category:"奨学金",year:2021,month:5,amount:3)
