10777f9c325eb54303f45b6dca73019082b9780b
nao.saino <saino305@gmail.com>
2020-09-19 14:39:24 +0000

    before_action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController<ApplicationController
+    
+    before_action :set_book, only:[:show, :edit, :update, :destroy]
+    
     def index
         @books=Book.all
     end
     
     def show
-        @book = Book.find(params[:id])
     end
     
     def new
@@ -24,11 +26,9 @@ class BooksController<ApplicationController
     end
     
     def edit
-        @book = Book.find(params[:id])
     end
     
     def update
-        @book = Book.find(params[:id])
         book_params=params.require(:book).permit(:year,:month,:inout,:category,:amount)
         if @book.update(book_params)
         flash[:notice] = "更新しました。"
@@ -40,10 +40,13 @@ class BooksController<ApplicationController
     end
     
     def destroy
-        @book = Book.find(params[:id])
         @book.destroy
         flash[:notice] = "削除しました。"
         redirect_to books_path
+    end
+    
+    def set_book
+        @book = Book.find(params[:id])
     end
     
 end
